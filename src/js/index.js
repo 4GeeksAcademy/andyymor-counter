b26156415573cbe369097be842f1b8768eabe45f
andyymor <61852990+andyymor@users.noreply.github.com>
2023-12-05 19:42:40 +0000

    second commmit

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,35 @@
 //import react into the bundle
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import propTypes from "prop-types";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 
-function SimpleCounter(props) {
-    return( <div className="bigCounter">
+function SimpleCounter() {
+    let [counter, setCounter] = useState(0);
+   useEffect(() =>{
+    setInterval(function () {
+        setCounter(counter++)
+    }, 1000);
+   },[])
+    return (<div className="bigCounter">
         <div className="calendar">
             <i class="fa-solid fa-clock"></i>
         </div>
-        <div className="four">{props.digitFour % 10}</div>
-        <div className="three">{props.digitThree % 10}</div>
-        <div className="two">{props.digitTwo % 10}</div>
-        <div className="one">{props.digitOne % 10}</div>
+        <div className="four">{Math.floor(counter/1000) % 10}</div>
+        <div className="three">{Math.floor(counter/100) % 10}</div>
+        <div className="two">{Math.floor(counter/10) % 10}</div>
+        <div className="one">{Math.floor(counter/1) % 10}</div>
     </div>);
+
 }
-SimpleCounter.propTypes = {
-    digitFour: PropTypes.number,
-    digitThree: PropTypes.number,
-    digitTwo: PropTypes.number,
-    digitOne: PropTypes.number
-};
 
 
-let counter = 0;
-setInterval(function(){
-const four = Math.floor(counter/1000);
-const three = Math.floor(counter/100);
-const two = Math.floor(counter/10);
-const one = Math.floor(counter/1);
-counter++;
-}, 1000);
+
+
 //render your react application
 
-ReactDOM.render(<SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />,
- document.querySelector("#app")
- );
+ReactDOM.render(<SimpleCounter  />,
+    document.querySelector("#app")
+);
